1144deae10b88c3855538163980cb6ed2ce179a5
Charlie Drage <charlie@charliedrage.com>
2016-09-08 08:25:37 -0400

    Update example

--- examples/kubernetes.py
+++ examples/kubernetes.py
@@ -3,5 +3,15 @@ import kubeshift
 
 user = getpass.getuser()
 
-a = kubeshift.Client(kubeshift.Config.from_file("/home/%s/.kube/config" % user), "kubernetes")
-print a.namespaces()
+client = kubeshift.Client(kubeshift.Config.from_file("/home/%s/.kube/config" % user), "kubernetes")
+
+# Print out all available namespaces
+print client.namespaces()
+
+k8s_object = {"apiVersion": "v1", "kind": "Pod", "metadata": {"labels": {"app": "hellonginx"}, "name": "hellonginx"}, "spec": {"containers": [{"image": "nginx", "name": "hellonginx", "ports": [{"containerPort": 80, "hostPort": 80, "protocol": "TCP"}]}]}}
+
+# Create the object
+client.create(k8s_object)
+
+# Delete the object
+# client.delete(k8s_object)
